39cef21255b4d26602df1728130883d25aa62c2e
Damià Arnau Pont Pons <damiaarnau.pontpons@yougov.com>
2022-05-23 14:54:31 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="test-div">container</div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  height: 500vh;
+}
+.test-div {
+  border: 2px solid red;
+  position: fixed;
+  inset: 0;
+  background: hsla(120, 100%, 50%, 0.2);
+}
+</style>
